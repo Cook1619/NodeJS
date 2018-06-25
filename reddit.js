d97d3078e30ac6357c218fa3e7f5d30ae666433c
Cook1619 <cook6348@gmail.com>
2018-06-25 13:46:45 -0500

    pushed to an array

--- reddit.js
+++ reddit.js
@@ -7,7 +7,10 @@ let dataPath = path.join(__dirname, './popular-articles.json')
 rp('https://reddit.com/r/popular.json')
     .then((res) => {
        JSON.parse(res).data.children.forEach(item => {
-           fs.appendFileSync(dataPath, item.data.title + '\n' + item.data.url + '\n' + item.data.author);
+           let newArr = [];
+        //    fs.appendFileSync(dataPath, item.data.title + '\n' + item.data.url + '\n' + item.data.author);
+        newArr.push(item.data.title, item.data.url, item.data.author);
+              fs.appendFileSync(dataPath, newArr + '\n');
        })
         
     }).catch((err) =>{
