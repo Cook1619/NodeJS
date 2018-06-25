915eb286fc989bc1368d876913f040fd83c7b970
Cook1619 <cook6348@gmail.com>
2018-06-25 12:43:38 -0500

    Looping through the fields i want

--- reddit.js
+++ reddit.js
@@ -1,9 +1,15 @@
-const fs = require(fs);
+const fs = require('fs');
 const rp = require('request-promise');
 
 
 rp('https://reddit.com/r/popular.json')
     .then((res) => {
-        console.log(res);
+       JSON.parse(res).data.children.forEach(item => {
+           console.log(item.data.title)
+           console.log(item.data.url);
+           console.log(item.data.author);
+       })
+        
+    }).catch((err) =>{
         console.log(err);
     })
