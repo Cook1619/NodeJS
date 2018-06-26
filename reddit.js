ef49eb98913ca92c0efe80bcac6d8c457715a3cb
Cook1619 <cook6348@gmail.com>
2018-06-26 12:42:19 -0500

    still getting the end of file

--- reddit.js
+++ reddit.js
@@ -6,13 +6,11 @@ let dataPath = path.join(__dirname, './popular-articles.json')
 
 rp('https://reddit.com/r/popular.json')
     .then((res) => {
-
-        JSON.parse(res).data.children.forEach(item => {
-            let newObj = { title: item.data.title, url: item.data.url, author: item.data.author }
-            let newArr = [];
-            newArr.push(newObj);
-            fs.appendFileSync(dataPath, JSON.stringify(newArr) + '\n');
-        })
+        const items = 
+        JSON.parse(res).data.children.map(item => {
+           return {title: item.data.title, url: item.data.url, author: item.data.author}
+            fs.writeFile(dataPath, JSON.stringify(items) + '\n');
+        });
 
     }).catch((err) => {
         console.log(err);
